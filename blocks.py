df131d5388a900f9d3a7026bc512432958ce1dd7
Sam <sam.nagourney@gmail.com>
2018-12-25 14:35:15 -0500

    Re-did oxford comma

--- blocks.py
+++ blocks.py
@@ -1,4 +1,42 @@
-name = 'Alice'
-if name == 'Alice':
-    print('Hi Alice')
-else print('Hi Not Alice')
+# name = 'Bob'
+# if name == 'Alice':
+#     print('Hi Alice')
+# elif name == 'Bob':
+#     print("Hi Bob")
+# else:
+#     print('Hi Not Alice')
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1  
+# else:
+#     print('Done with count')
+
+# while True: 
+#     print("Please type your name")
+#     name = raw_input()
+#     if name != 'your name':
+#         continue
+#     print('Hey your name, what\'s good?')
+#     break
+# print('Thank you')
+
+
+
+# print('My name is:')
+# for i in range(0, 10, 2):
+#     print('Sam' + ' ' + str(i))
+
+
+# import random
+# for i in range(int('10')):
+#     print(random.randint(1, 10))
+
+while True:
+    print('Type \'stop\' to exit')
+    value = raw_input()
+    if value == 'stop':
+        sys.exit()
+    print('Have a good day')
+
